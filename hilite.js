18cec7f477b0c319b9919c48514378072c66c186
cygx <cygx@cpan.org>
2019-07-21 14:09:21 +0200

    fix highlighter treatment of &

--- hilite.js
+++ hilite.js
@@ -3,12 +3,12 @@ function hilite(code) {
     const VAR = '<span class="var">$&</span>';
     const KEY = '<span class="key">$&</span>';
 
-    return code.replace('&', '&#38;')
-               .replace('<', '&#60;')
-               .replace('>', '&#62;')
+    return code.replace('&', '&amp;')
+               .replace('<', '&lt;')
+               .replace('>', '&gt;')
                .replace(/"(\\.|[^"])*"/g, LIT)
                .replace(/'(\\.|[^'])*'/g, LIT)
-               .replace(/[\$\&\%\@]\^?\w+/g, VAR)
+               .replace(/(?:[\$\%\@]|\&amp;)\^?\w+/g, VAR)
                .replace(/\b\d+/g, LIT)
                .replace(/(^|\n)\s*(sub|proto|multi|my|state|given|for)\b/g, KEY)
                .replace(/\s+for\s+/g, KEY);
